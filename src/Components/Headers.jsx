92b785c8253e4edb58f9f9a5221102fb8b0501ad
serududuy1 <serududuy@gmail.com>
2023-01-09 02:59:04 +0700

    add validation login

--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -1,33 +1,41 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { fetchUserId } from "../Redux/user"
-import Logo from "./assets/img/adopt-me-logo.png"
-import Logos from "./assets/img/logos.png"
-import "./assets/headers.css"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { fetchUserId } from "../Redux/user";
+import Logo from "./assets/img/adopt-me-logo.png";
+import Logos from "./assets/img/logos.png";
+import "./assets/headers.css";
+import Swal from "sweetalert2";
 
 const Headers = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const ls = localStorage.getItem("token")
-  const dt = useSelector((state) => state)
-  console.log(dt)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const ls = localStorage.getItem("token");
+  const dt = useSelector((state) => state);
+  console.log(dt);
 
   const handleNavbarLogin = () => {
-    ls ? navigate("/profile") : navigate("/login")
-  }
+    ls ? navigate("/profile") : navigate("/login");
+  };
 
   const cek = () => {
     dispatch(fetchUserId()).then((res) => {
-      //  navigate("/profile");
-    })
-  }
+      console.log(res);
+      if (res.payload.name === "AxiosError") {
+        Swal.fire({
+          title: "Sesi anda telah berakhir!",
+          text: "Silahkan Login",
+          icon: "error",
+        }).then((response) => navigate("/login"));
+      }
+    });
+  };
   const home = () => {
-    navigate("/home")
-  }
+    navigate("/home");
+  };
   useEffect(() => {
-    cek()
-  }, [])
+    cek();
+  }, []);
 
   return (
     <div className="header">
@@ -39,6 +47,6 @@ const Headers = () => {
         onClick={() => handleNavbarLogin()}
       ></img>
     </div>
-  )
-}
-export default Headers
+  );
+};
+export default Headers;
