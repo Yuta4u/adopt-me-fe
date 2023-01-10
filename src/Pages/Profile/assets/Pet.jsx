0bb193516952229734ddd6fa5a524eec0d45ab67
serududuy1 <serududuy@gmail.com>
2023-01-10 11:32:18 +0700

    add something:

--- src/Pages/Profile/assets/Pet.jsx
+++ src/Pages/Profile/assets/Pet.jsx
@@ -36,16 +36,9 @@ const Paginated = ({ itemsPerPage }) => {
       setDataAnimal(res.payload.data)
     );
   }, [animal]);
-  console.log(dataAnimal);
-  // Simulate fetching items from another resources.
-  // (This could be items from props; or items loaded in a local state
-  // from an API endpoint with useEffect and useState)
   const endOffset = itemOffset + itemsPerPage;
-  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
   const currentItems = dataAnimal.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(dataAnimal.length / itemsPerPage);
-  console.log(currentItems);
-  // Invoke when user click to request another page.
   const handlePageClick = (event) => {
     const newOffset = (event.selected * itemsPerPage) % dataAnimal.length;
     console.log(
@@ -61,22 +54,11 @@ const Paginated = ({ itemsPerPage }) => {
           <div className="col-md-12 mt-2">
             <div className="row gap-3">
               <Items currentItems={currentItems} />
-              {/* {dataAnimal.map((e) => (
-              <div class="carreaux_presentation_light lel">
-                <div class="shadow_swhow_mini lel">
-                  <img
-                    src={`http://localhost:8000/${e.images.replace(`\\`, "/")}`}
-                    className="pet-card-img"
-                  />
-                  <div class="deroul_titre">{e.name}</div>
-                  <div class="deroul_soustitre">{e.jenis}</div>
-                </div>
-              </div>
-            ))} */}
             </div>
           </div>
         </div>
       </div>
+
       <div className="btn-pagination">
         <ReactPaginate
           className="d-flex justify-content-between "
